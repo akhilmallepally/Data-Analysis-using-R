ee0818ef9d400b59253eb6801c9d6d6daf96acc3
akhilmallepally <akhilnani831@gmail.com>
2021-04-24 15:49:21 -0500

    vectors and lists
    variables, functions, comments, vectors, lists

---
+++ fundamentals.R
@@ -0,0 +1,19 @@
+
+sales_1 <- 243.55
+sales_2 <- 345.22
+
+mid_sales <- sales_1 + sales_1
+print(mid_sales)
+
+
+vect1 <- c(1,2,4.0)
+names(vect1) <- c("a","b","c")
+print(vect1)
+
+lst1 <- list(1,"hello","tst",TRUE,10.4 )
+print(lst1)
+
+lst2 = list("hello"=1, "bye"=  2, "test"=3)
+print(lst2)
+#installed.package()
+#library("tidyverse")
